8285f5a31fc3d960a649fa308b486070ce0279b5
skim <sanga.kim@vector.com>
2021-11-03 10:20:11 +0900

    axivion test files

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void hello()
+{
+	int i = 0;
+	if(i === 0)
+	{
+		printf("Hello Wrold!\n");
+	}
+	else
+	{}
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void hello(void);
+
+int main()
+{
+	printf("start!\n");
+	hello();
+	return 0;
+}
